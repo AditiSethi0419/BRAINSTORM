330d3128871ae9c15a00af6aab19e50f2351f104
Sagar Gupta <sagar.gupta@novoinvent.com>
2023-04-07 21:59:32 +0530

    ImageSlide changes

---
+++ src/Components/ImageSlideData.js
@@ -0,0 +1,15 @@
+import Image1 from '../Images/Image1.jpg';
+import Image2 from '../Images/Image2.png';
+import Image3 from '../Images/Image3.jpg';
+import Image4 from '../Images/Image4.png';
+import Image5 from '../Images/Image5.jpg';
+
+const slides =[
+    {title:'Image1',img:{Image1},description:'I am image1'},
+    {title:'Image2',img:{Image2},description:'I am image 2'},
+    {title:'Image3',img:{Image3},description:'I am image 3'},
+    {title:'Image4',img:{Image4},description:'I am image 4'},
+    {title:'Image5',img:{Image5},description:'I am image 5'},
+];
+
+export default slides;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,15 +6,34 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import blog_logo from '../Images/blog_logo.jpg';
+import ImageSlide from './ImageSlide';
+import Image1 from '../Images/Image1.jpg';
+import Image2 from '../Images/Image2.png';
+import Image3 from '../Images/Image3.jpg';
+import Image4 from '../Images/Image4.png';
+import Image5 from '../Images/Image5.jpg';
+import ImgMediaCard from './ImgMediaCard';
+import Footer from './Footer';
 
-
+const slides =[
+  {title:'Image1',img:{Image1},description:'I am image1'},
+  {title:'Image2',img:{Image2},description:'I am image2'},
+  {title:'Image3',img:{Image3},description:'I am image3'},
+  {title:'Image4',img:{Image4},description:'I am image4'},
+  {title:'Image5',img:{Image5},description:'I am image5'},
+];
 function Navbar() {
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+  };
   return (<>
    <Box sx={{ flexGrow: 1 }}>
     <AppBar position="static" sx={{ bgcolor: "white",color:'black',textAlign:'right' }} >
       <Container maxWidth="xl">
         <Toolbar >
-        <img src={blog_logo} style={{width:'50px',height:'50px'}}  />
+        <img src={blog_logo} style={{width:'50px',height:'50px'}}  alt='logo_here'/>
           <Typography
             variant="h6"
             noWrap
@@ -28,12 +47,9 @@ function Navbar() {
               letterSpacing: '.3rem',
               color: 'inherit',
               textDecoration: 'none',
-            }}
-          >
+            }}>
             BRAINSTORM
-           
-          </Typography>
-          
+           </Typography>
           <Box  sx={{flexGrow: 1,color:'black'}}>
           <Button color="inherit">Write</Button>
           <Button color="inherit">Sign In</Button>
@@ -42,7 +58,17 @@ function Navbar() {
       </Container>
     </AppBar>
     </Box>
+    <div style={containerStyles}>
+        <ImageSlide slides={slides} />
+      </div>
+    <br></br>
+    <br></br>
    
+      <ImgMediaCard />
+   <Footer />
+      
+      
+      
     </>
   );
 }
